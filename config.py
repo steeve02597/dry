81d6a8e2ed5588006c9043a4ebf0fccd552bf3a4
steeve02597 <119192572+steeve02597@users.noreply.github.com>
2022-12-02 23:12:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "Don't share messages here..Check @seriescc ,select series & season then press start.!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
